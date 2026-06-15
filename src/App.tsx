c6b828616f60fece1b85d2ad1a787466e661bf61
Divyanshu951 <divyanshuk9515@gmail.com>
2026-06-16 00:31:08 +0530

    init react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./AppLayout";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Settings from "./pages/Settings";
+import Error from "./Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Landing />,
+      },
+      { path: "/login", element: <Login /> },
+      { path: "/signup", element: <Signup /> },
+      { path: "/settings", element: <Settings /> },
+    ],
+  },
+]);
+
 function App() {
-  return <h1 className="p-4 pt-2">Hello</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+
+/**
+ * PLAN -
+ *
+ * react router for /, /login, /signup, /settings
+ * style using tailwind
+ *
+ */
